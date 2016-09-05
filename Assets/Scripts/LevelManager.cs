d660b9f93224ea2d0201aa127cead7af1f88329f
Anders Tolborg <anders@tolborg.dk>
2016-09-05 22:25:59 +0200

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void LoadLevel(string name){
+		SceneManager.LoadScene(name);
+	}
+
+
+	public void QuitRequest(){
+		Application.Quit();
+	}
+
+
+	public void LoadNextLevel() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+
+}
