5721f3a64862b9847877a99061e2f64000430933
Anders Tolborg <anderstolborg@sybogames.com>
2016-09-06 15:21:53 +0200

    blah

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+		SceneManager.sceneLoaded += PlayLevelMusic;
+	}
+		
+
+	void PlayLevelMusic(Scene scene, LoadSceneMode loadSceneMode) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[scene.buildIndex];
+
+		if (thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+
+
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,6 +5,16 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 
+	public float autoLoadNextLevelAfter;
+
+
+	void Start() {
+		if (autoLoadNextLevelAfter > 0) {
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+
 	public void LoadLevel(string name){
 		SceneManager.LoadScene(name);
 	}
